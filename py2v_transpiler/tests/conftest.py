6bc34ea433f32c55decfdb226b049d1ab889eacc
Yaskhan <yaskhan@yandex.ru>
2026-03-07 01:41:23 +0600

    Ignore tests in input/transpile

--- py2v_transpiler/tests/conftest.py
+++ py2v_transpiler/tests/conftest.py
@@ -7,16 +7,16 @@ import pytest
 
 
 def pytest_collection_modifyitems(config, items):
-    """Modify collected items to exclude tests from cpython directory."""
-    # Filter out tests from cpython directory
+    """Modify collected items to exclude tests from cpython and transpile directories."""
+    # Filter out tests from cpython and transpile directories
     items[:] = [
         item for item in items
-        if "cpython" not in str(item.fspath)
+        if "cpython" not in str(item.fspath) and "transpile" not in str(item.fspath)
     ]
 
 
 def pytest_ignore_collect(collection_path, config):
-    """Ignore the cpython directory during test collection."""
-    if "cpython" in str(collection_path):
+    """Ignore the cpython and transpile directories during test collection."""
+    if "cpython" in str(collection_path) or "transpile" in str(collection_path):
         return True
     return None
